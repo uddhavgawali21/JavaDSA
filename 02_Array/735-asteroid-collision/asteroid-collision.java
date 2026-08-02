71e8a79d9ac1a258f521ee0af15de0756e406849
uddhavgawali21 <146969862+uddhavgawali21@users.noreply.github.com>
2026-08-02 19:55:18 +0530

    Time: 5 ms (68.75%) | Memory: 46.7 MB (84.48%) - LeetSync

--- 02_Array/735-asteroid-collision/asteroid-collision.java
+++ 02_Array/735-asteroid-collision/asteroid-collision.java
@@ -1,28 +1,67 @@
+// class Solution {
+//     public int[] asteroidCollision(int[] asteroids) {
+//         Stack<Integer> st = new Stack<>();
+
+//         for(int i : asteroids){
+//             while(!st.isEmpty() && st.peek()>0 && i<0 && st.peek()< -i){
+//                     st.pop();
+                    
+//                 }
+
+//             if(!st.isEmpty() && st.peek()>0 && i<0){
+                
+//                 if(Math.abs(st.peek())==Math.abs(i)){
+//                     st.pop();
+//                 }
+//             }
+//             else{
+//                 st.push(i);
+//             }
+//         }
+//         int[] arr = new int[st.size()];
+
+//         for(int i = arr.length-1; i>=0 ; i--){
+//             arr[i] = st.pop();
+//         }
+//         return arr;
+//     }
+// }
+
+
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        Stack<Integer> st = new Stack<>();
 
-        for(int i : asteroids){
-            while(!st.isEmpty() && st.peek()>0 && i<0 && st.peek()< -i){
-                    st.pop();
-                    
+        Stack<Integer> stack = new Stack<>();
+
+        for (int asteroid : asteroids) {
+
+            while (!stack.isEmpty() && asteroid < 0 && stack.peek() > 0) {
+
+                if (stack.peek() < -asteroid) {
+                    stack.pop();
                 }
-
-            if(!st.isEmpty() && st.peek()>0 && i<0){
-                
-                if(Math.abs(st.peek())==Math.abs(i)){
-                    st.pop();
+                else if (stack.peek() == -asteroid) {
+                    stack.pop();
+                    asteroid = 0;
+                    break;
+                }
+                else {
+                    asteroid = 0;
+                    break;
                 }
             }
-            else{
-                st.push(i);
+
+            if (asteroid != 0) {
+                stack.push(asteroid);
             }
         }
-        int[] arr = new int[st.size()];
 
-        for(int i = arr.length-1; i>=0 ; i--){
-            arr[i] = st.pop();
+        int[] ans = new int[stack.size()];
+
+        for (int i = ans.length - 1; i >= 0; i--) {
+            ans[i] = stack.pop();
         }
-        return arr;
+
+        return ans;
     }
 }
